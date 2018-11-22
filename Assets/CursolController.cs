673d41fcc8f519c3f36fc92a4ae8828c36fc07c7
user224 <toda7.mk3@gmail.com>
2018-11-22 14:44:15 +0900

    最初のコミット 2018-11-22

---
+++ Assets/CursolController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursolController : MonoBehaviour {
+
+    public GameObject mushroomPrefab;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(Camera.main.transform.position,
+                    Camera.main.transform.rotation * Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.name == "Platform1")
+            {
+                transform.position = hit.point + new Vector3(0, 0.1f, 0);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = transform.position;
+            pos.y = -0.5f;
+            Instantiate(mushroomPrefab, pos, Quaternion.identity);
+        }
+
+
+    }
+}
